178b73e35f75c5dd402731362416340ada6152ae
Mattis Kjellerås <77880455+INeedAwesome@users.noreply.github.com>
2022-12-03 14:00:20 +0100

    Initial commit

---
+++ TemplateApp/premake5.lua
@@ -0,0 +1,43 @@
+project "TemplateApp"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+   targetdir "bin/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   pchheader "stdafx.h"
+   pchsource "../%{prj.name}/src/stdafx.cpp"
+
+   files { "src/**.h", "src/**.cpp" }
+
+   includedirs
+   {
+   }
+
+   targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+   objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+   filter "system:windows"
+      systemversion "latest"
+      defines { "WL_PLATFORM_WINDOWS" }
+   filter "system:linux"
+      systemversion "latest"
+      defines { "WL_PLATFORM_LINUX" }
+
+   filter "configurations:Debug"
+      defines { "WL_DEBUG" }
+      runtime "Debug"
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "WL_RELEASE" }
+      runtime "Release"
+      optimize "On"
+      symbols "On"
+
+   filter "configurations:Dist"
+      kind "WindowedApp"
+      defines { "WL_DIST" }
+      runtime "Release"
+      optimize "On"
+      symbols "Off"
